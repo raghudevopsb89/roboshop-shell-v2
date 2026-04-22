5812ecc4218a40099358c5af583ce0364bf74a9c
Raghu K <raghuk.vit@gmail.com>
2026-04-22 06:56:40 +0530

    RoboShop Code

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,13 @@
 source common.sh
 
-echo -e "${hs} Install Nginx Config ${he}" | tee -a ${log_file}
+echo -e "${hs} Install MySQL Server ${he}" | tee -a ${log_file}
 dnf install -y mysql8.4-server &>>${log_file}
 
-echo -e "${hs} Copy Nginx Config ${he}" | tee -a ${log_file}
+echo -e "${hs} Start MySQL Server ${he}" | tee -a ${log_file}
 systemctl enable mysqld &>>${log_file}
 systemctl start mysqld &>>${log_file}
 
-echo -e "${hs} Copy Nginx Config ${he}" | tee -a ${log_file}
+echo -e "${hs} Setup MySQL Root Password ${he}" | tee -a ${log_file}
 mysql -u root -e "
   CREATE USER 'root'@'%' IDENTIFIED BY 'RoboShop@1';
   GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
