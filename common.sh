b249f35756dbd8f00cdb8b86d9e64db5667297d3
Raghu K <raghuk.vit@gmail.com>
2026-04-23 06:31:05 +0530

    RoboShop Code

--- common.sh
+++ common.sh
@@ -69,3 +69,12 @@ golang_app() {
 
   systemd_service
 }
+
+status_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
+}
+
--- frontend.sh
+++ frontend.sh
@@ -4,44 +4,44 @@ component_name=frontend
 echo Log file Output : ${log_file}
 echo -e "${hs} Install Nginx ${he}" | tee -a ${log_file}
 dnf install -y nginx &>>${log_file}
-echo $?
+status_check
 
 echo -e "${hs} Copy Nginx Config ${he}" | tee -a ${log_file}
 cp nginx.conf /etc/nginx/nginx.conf &>>${log_file}
-echo $?
+status_check
 
 echo -e "${hs} Install NodeJS ${he}" | tee -a ${log_file}
 curl -fsSL https://rpm.nodesource.com/setup_20.x | bash - &>>${log_file}
 dnf install -y nodejs &>>${log_file}
-echo $?
+status_check
 
 echo -e "${hs} Download ${component_name} Code ${he}" | tee -a ${log_file}
 curl -L -o /tmp/${component_name}.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/${component_name}.zip &>>${log_file}
-echo $?
+status_check
 
 echo -e "${hs} Create App Directory ${he}" | tee -a ${log_file}
 rm -rf /tmp/${component_name} &>>${log_file}
 mkdir -p /tmp/${component_name} &>>${log_file}
-echo $?
+status_check
 cd /tmp/${component_name} &>>${log_file}
 
 echo -e "${hs} Extract App Code ${he}" | tee -a ${log_file}
 unzip /tmp/${component_name}.zip &>>${log_file}
-echo $?
+status_check
 
 echo -e "${hs} Install App Dependencies & Build Html Code ${he}" | tee -a ${log_file}
 npm cache clean --force &>>${log_file}
 npm install &>>${log_file}
 npm run build &>>${log_file}
-echo $?
+status_check
 
 echo -e "${hs} Copy Built Code to Nginx ${he}" | tee -a ${log_file}
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 cp -r out/* /usr/share/nginx/html/ &>>${log_file}
-echo $?
+status_check
 
 echo -e "${hs} Start Nginx Service ${he}" | tee -a ${log_file}
 systemctl enable nginx &>>${log_file}
 systemctl restart nginx &>>${log_file}
-echo $?
+status_check
 
