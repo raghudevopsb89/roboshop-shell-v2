2905ef2d1fbb7209748534002afa6a4c50192881
Raghu K <raghuk.vit@gmail.com>
2026-04-23 06:39:53 +0530

    RoboShop Code

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,8 @@
 source common.sh
 component_name=catalogue
 echo Log file Output : ${log_file}
-
-echo -e "${hs} Install MySQL Client ${he}" | tee -a ${log_file}
-dnf install mysql8.4 -y &>>${log_file}
-status_check
-
-echo -e "${hs} Load Schema, App User, & Master Data ${he}" | tee -a ${log_file}
-mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 < db/schema.sql &>>${log_file}
-mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 < db/app-user.sql &>>${log_file}
-mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 ${component_name} < db/master-data.sql &>>${log_file}
-status_check
+schema_load=true
+schema_type=mysql
 
 golang_app
 
--- common.sh
+++ common.sh
@@ -31,6 +31,8 @@ app_pre_reqs() {
 }
 
 systemd_service() {
+  schema_load
+
   echo -e "${hs} Apply permission for application folder  ${he}" | tee -a ${log_file}
   chown -R appuser:appuser /app &>>${log_file}
   chmod o-rwx /app -R &>>${log_file}
@@ -78,6 +80,22 @@ status_check() {
     echo -e "\e[32m SUCCESS \e[0m"
   else
     echo -e "\e[31m FAILURE \e[0m"
+  fi
+}
+
+schema_load() {
+  if [ "$schema_load" = "true" ]; then
+    if [ "$schema_type" = "mysql" ]; then
+      echo -e "${hs} Install MySQL Client ${he}" | tee -a ${log_file}
+      dnf install mysql8.4 -y &>>${log_file}
+      status_check
+
+      echo -e "${hs} Load Schema, App User, & Master Data ${he}" | tee -a ${log_file}
+      mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 < db/schema.sql &>>${log_file}
+      mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 < db/app-user.sql &>>${log_file}
+      mysql -h mysql-dev.rdevopsb89.online -u root -pRoboShop@1 ${component_name} < db/master-data.sql &>>${log_file}
+      status_check
+    fi
   fi
 }
 
